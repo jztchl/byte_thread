068d21dfe01c4627ef9e48450ec6a69752f1711a
jztchl <kmvishnu625@gmail.com>
2025-07-05 22:02:35 +0530

    used abstract models in thread and its siblings

--- threads/models.py
+++ threads/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from users.models import User
-
+from core.models import SoftDelete
+from core.models import Timestamp
 class CommentType(models.TextChoices):
    TEXT = "text", "Text"
    IMAGE = "image", "Image"
@@ -17,48 +18,39 @@ class Reactions(models.TextChoices):
    LAUGH = "laugh", "Laugh"
 
 
-class Thread(models.Model):
+class Thread(SoftDelete,Timestamp):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.TextField()
     images = models.ManyToManyField("core.Image", blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
     def __str__(self):
         return self.title
 
-class Comment(models.Model):
+class Comment(SoftDelete,Timestamp):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     comment_type = models.CharField(max_length=10, choices=CommentType.choices)
     thread = models.ForeignKey(Thread, on_delete=models.CASCADE, related_name='comments')
     content = models.TextField()
     images = models.ForeignKey("core.Image", on_delete=models.SET_NULL, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.content
 
-class Reply(models.Model):
+class Reply(SoftDelete,Timestamp):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='replies')
     content = models.TextField()
     comment_type = models.CharField(max_length=10, choices=CommentType.choices)
     images = models.ForeignKey("core.Image", on_delete=models.SET_NULL, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.content
 
 
-class ThreadReactions(models.Model):
+class ThreadReactions(Timestamp):
     thread = models.ForeignKey(Thread, on_delete=models.CASCADE, related_name='reactions')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     reaction = models.CharField(max_length=10, choices=Reactions.choices)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.user} {self.reaction} {self.thread}"
@@ -66,12 +58,10 @@ class ThreadReactions(models.Model):
         unique_together = ('thread', 'user')
 
 
-class CommentReactions(models.Model):
+class CommentReactions(Timestamp):
     comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='reactions')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     reaction = models.CharField(max_length=10, choices=Reactions.choices)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.user} {self.reaction} {self.comment}"
@@ -79,12 +69,10 @@ class CommentReactions(models.Model):
         unique_together = ('comment', 'user')
 
 
-class ReplyReactions(models.Model):
+class ReplyReactions(Timestamp):
     reply = models.ForeignKey(Reply, on_delete=models.CASCADE, related_name='reactions')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     reaction = models.CharField(max_length=10, choices=Reactions.choices)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.user} {self.reaction} {self.reply}"
